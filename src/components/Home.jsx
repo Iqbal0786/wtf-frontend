79912bdf4076d3c5fdcdaced2b711dc43e391b4b
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-25 13:43:36 +0530

    added navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar } from '@mui/material'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <AppBar>
+        <Toolbar>
+            Home
+        </Toolbar>
+    </AppBar>
+    </>
+  )
+}
