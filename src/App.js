77b6b608368cb28dc91544b6bc81e1545dc65611
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-25 14:11:05 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import AllRoutes from './AllRoutes/AllRoutes';
 
 function App() {
   return (
     <>
-    <Home/>
+   <AllRoutes/>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,34 @@
-import { AppBar, Toolbar } from '@mui/material'
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <>
-    <AppBar>
+    <AppBar sx={{position:"" , backgroundColor:"#0d0d0d" , color:"white"}}>
         <Toolbar>
             Home
+            <Box sx={{marginLeft:"auto" , width:"60%" , color:"white" , display:"flex" , justifyContent:"space-between" ,paddingTop:"1%"}}>
+              <Link to={""} style={{textDecoration:"none" , color:"whitesmoke"}}>
+              <Typography>Fitness</Typography>
+              </Link>
+              <Link to={""} style={{textDecoration:"none" , color:"whitesmoke"}}>
+              <Typography>Fitness</Typography>
+              </Link>
+              <Link to={""} style={{textDecoration:"none" , color:"whitesmoke"}}>
+              <Typography>Fitness</Typography>
+              </Link>
+              <Link to={""} style={{textDecoration:"none" , color:"whitesmoke"}}>
+              <Typography>Fitness</Typography>
+              </Link>
+              <Link to={""} style={{textDecoration:"none" , color:"whitesmoke"}}>
+              <Typography>Fitness</Typography>
+              </Link>
+              <Link to={""} style={{textDecoration:"none" , color:"whitesmoke"}}>
+              <Button sx={{marginTop:"-6px" , backgroundColor:"#d50202" , color:"whitesmoke"}}>Fitness</Button>
+              </Link>
+               
+            </Box>
         </Toolbar>
     </AppBar>
     </>
